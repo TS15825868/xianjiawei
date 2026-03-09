bcfefed91d127163befba4880f0c742411dbe5b2
TS15825868 <tung314069@gmail.com>
2026-03-09 17:54:49 +0800

    Add files via upload

--- site.js
+++ site.js
@@ -1,10 +1,1 @@
-
-const drawer=document.getElementById('drawer');
-function toggleMenu(){drawer.classList.toggle('open');}
-document.addEventListener('click',e=>{
- if(e.target.matches('#drawer a'))drawer.classList.remove('open');
- if(!e.target.closest('#drawer')&&!e.target.closest('.menu'))drawer.classList.remove('open');
-});
-document.addEventListener('keydown',e=>{
- if(e.key==='Escape')drawer.classList.remove('open');
-});
+function toggleMenu(){}
