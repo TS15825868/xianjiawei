d61ceb1e1e03bceaf2eb8cbb1e1fe86e83c79ad7
TS15825868 <tung314069@gmail.com>
2026-03-17 23:26:30 +0800

    Add files via upload

--- internal-link.js
+++ internal-link.js
@@ -1,48 +1,6 @@
-document.addEventListener("DOMContentLoaded", () => {
-
-const KEYWORDS = [
-{keyword:"龜鹿",url:"../articles/what-is-guilu.html"},
-{keyword:"鹿茸",url:"../articles/deer-antler-what.html"},
-{keyword:"龜鹿膏",url:"../product.html?id=guilu-gao"},
-{keyword:"龜鹿飲",url:"../product.html?id=guilu-drink"},
-{keyword:"龜鹿湯塊",url:"../product.html?id=guilu-block"},
-{keyword:"鹿茸粉",url:"../product.html?id=lurong-powder"}
-];
-
-function autoLink(container){
-
-if(!container) return;
-
-let html = container.innerHTML;
-
-KEYWORDS.forEach(item => {
-
-const regex = new RegExp(item.keyword);
-
-html = html.replace(regex, `<a href="${item.url}" class="seo-link">${item.keyword}</a>`);
-
-});
-
-container.innerHTML = html;
-
-}
-
-const article = document.querySelector("article");
-
-if(article){
-autoLink(article);
-}
-
-const style = document.createElement("style");
-
-style.innerHTML = `
-.seo-link{
-color:#8A1F1F;
-text-decoration:underline;
-font-weight:500;
-}
-`;
-
-document.head.appendChild(style);
-
+document.querySelectorAll("article p").forEach(p=>{
+p.innerHTML = p.innerHTML
+.replace(/龜鹿膏/g,'<a href="../product.html?id=guilu-gao">龜鹿膏</a>')
+.replace(/龜鹿飲/g,'<a href="../product.html?id=guilu-drink">龜鹿飲</a>')
+.replace(/龜鹿湯塊/g,'<a href="../product.html?id=guilu-block">龜鹿湯塊</a>');
 });
