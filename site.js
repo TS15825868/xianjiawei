d873b1e1b544c460c6a7a6347ceb1093332a3498
TS15825868 <tung314069@gmail.com>
2026-03-21 00:32:04 +0800

    Update site.js

--- site.js
+++ site.js
@@ -15,21 +15,18 @@ const btn=document.querySelector(".menu-btn");
 if(menu){
 menu.innerHTML=`
 <div class="menu-full">
+
 <div class="menu-close" onclick="toggleMenu()">✕</div>
 
-<div class="menu-block">
 <a href="index.html">首頁</a>
 <a href="brand.html">品牌</a>
-</div>
-
-<div class="menu-block">
 <a href="guilu-series.html">龜鹿系列</a>
 <a href="choose.html">怎麼選</a>
-</div>
-
-<div class="menu-block">
 <a href="faq.html">FAQ</a>
-</div>
+
+<a href="https://lin.ee/sHZW7NkR" class="btn btn-line">
+LINE詢問
+</a>
 
 </div>`;
 }
