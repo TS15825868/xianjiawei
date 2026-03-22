73d679a7f0e80081f2d04ef27e3621102af1a323
TS15825868 <tung314069@gmail.com>
2026-03-22 22:51:59 +0800

    Update site.js

--- site.js
+++ site.js
@@ -1,24 +1,16 @@
 document.addEventListener("DOMContentLoaded", () => {
 
-  const menuBtn = document.querySelector(".menu-btn");
-  const menuOverlay = document.getElementById("menuOverlay");
+  const btn = document.querySelector(".menu-btn");
+  const menu = document.getElementById("menuOverlay");
 
-  if(menuBtn){
-    menuBtn.addEventListener("click", ()=>{
-      menuOverlay.classList.add("active");
+  if(btn){
+    btn.addEventListener("click", ()=>{
+      menu.classList.add("active");
     });
   }
 
   window.closeMenu = function(){
-    menuOverlay.classList.remove("active");
-  }
-
-  if(menuOverlay){
-    menuOverlay.addEventListener("click", (e)=>{
-      if(e.target === menuOverlay){
-        closeMenu();
-      }
-    });
+    menu.classList.remove("active");
   }
 
 });
