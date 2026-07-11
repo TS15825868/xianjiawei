3141b4306a95d09697abcd587093228a5db3067a
TS15825868 <tung314069@gmail.com>
2026-07-11 21:00:50 +0800

    更新網站小老闆顯示快取 v324.2

--- site.js
+++ site.js
@@ -1,7 +1,6 @@
 "use strict";
 
-// 先載入網站專用小老闆素材，再載入網站核心內容。
-// 保留龜鹿飲 30cc 玻璃瓶與 180cc 鋁袋兩種規格。
-document.write('<link rel="stylesheet" href="site-v321.css?v=324.1" data-site-v324="1">');
-document.write('<script src="site-fix-v317.js?v=324.1"><\/script>');
-document.write('<script src="site-core.js?v=324.1"><\/script>');
+// 先載入網站小老闆情境圖，再載入網站核心內容。
+document.write('<link rel="stylesheet" href="site-v321.css?v=324.2" data-site-v324="1">');
+document.write('<script src="site-fix-v317.js?v=324.2"><\/script>');
+document.write('<script src="site-core.js?v=324.2"><\/script>');
