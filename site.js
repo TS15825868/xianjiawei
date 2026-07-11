ad59cf0b8e6addf6f7b4ead4d9109fbd27f43a5b
TS15825868 <tung314069@gmail.com>
2026-07-11 17:55:35 +0800

    更新全站共用資源快取版本 v322

--- site.js
+++ site.js
@@ -2,6 +2,6 @@
 
 // 共用正式程式與高畫質小老闆顯示修正。
 // 保留龜鹿飲 30cc 玻璃瓶與 180cc 鋁袋兩種規格。
-document.write('<link rel="stylesheet" href="site-v321.css?v=321.0" data-site-v321="1">');
-document.write('<script src="site-core.js?v=321.0"><\/script>');
-document.write('<script src="site-fix-v317.js?v=321.0"><\/script>');
+document.write('<link rel="stylesheet" href="site-v321.css?v=322.0" data-site-v322="1">');
+document.write('<script src="site-core.js?v=322.0"><\/script>');
+document.write('<script src="site-fix-v317.js?v=322.0"><\/script>');
