b2aa064c647ab2aa8d6041a4576cb7141b13e16b
TS15825868 <tung314069@gmail.com>
2026-07-11 21:13:27 +0800

    更新全站小老闆與核心程式快取 v325

--- site.js
+++ site.js
@@ -1,6 +1,6 @@
 "use strict";
 
-// 先載入網站小老闆情境圖，再載入網站核心內容。
-document.write('<link rel="stylesheet" href="site-v321.css?v=324.2" data-site-v324="1">');
-document.write('<script src="site-fix-v317.js?v=324.2"><\/script>');
-document.write('<script src="site-core.js?v=324.2"><\/script>');
+// 先載入網站專用小老闆情境圖，再載入網站核心內容。
+document.write('<link rel="stylesheet" href="site-v321.css?v=325.0" data-site-v325="1">');
+document.write('<script src="site-fix-v317.js?v=325.0"><\/script>');
+document.write('<script src="site-core.js?v=325.0"><\/script>');
