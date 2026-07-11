c14c63019875e4eebcc7f41b92546f725254cfe5
TS15825868 <tung314069@gmail.com>
2026-07-11 16:58:27 +0800

    更新全站共用程式與高畫質小老闆快取版本 v321

--- site.js
+++ site.js
@@ -1,6 +1,7 @@
 "use strict";
 
-// 網站正式版直接使用 images/brand 內的獨立 JPG。
+// 共用正式程式與高畫質小老闆顯示修正。
 // 保留龜鹿飲 30cc 玻璃瓶與 180cc 鋁袋兩種規格。
-document.write('<script src="site-core.js?v=318.0"><\/script>');
-document.write('<script src="site-fix-v317.js?v=318.0"><\/script>');
+document.write('<link rel="stylesheet" href="site-v321.css?v=321.0" data-site-v321="1">');
+document.write('<script src="site-core.js?v=321.0"><\/script>');
+document.write('<script src="site-fix-v317.js?v=321.0"><\/script>');
