a0d82185ec212b81c1eac8bc39f8500353fceb5c
TS15825868 <tung314069@gmail.com>
2026-07-11 15:37:59 +0800

    更新官網雙規格恢復版本 v317.2

--- site.js
+++ site.js
@@ -1,6 +1,6 @@
 "use strict";
 
 // 網站正式版直接使用 images/brand 內的獨立 JPG。
-// 不再使用 Blob／Base64 圖片精靈，避免 iPhone Safari 出現黑畫面。
-document.write('<script src="site-core.js?v=317.1"><\/script>');
-document.write('<script src="site-fix-v317.js?v=317.1"><\/script>');
+// 保留龜鹿飲 30cc 玻璃瓶與 180cc 鋁袋兩種規格。
+document.write('<script src="site-core.js?v=317.2"><\/script>');
+document.write('<script src="site-fix-v317.js?v=317.2"><\/script>');
