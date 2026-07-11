aa3b747ec6f3e2e768a4764a8184006d97b37d83
TS15825868 <tung314069@gmail.com>
2026-07-11 15:58:02 +0800

    更新網站小老闆修復快取版本 v318

--- site.js
+++ site.js
@@ -2,5 +2,5 @@
 
 // 網站正式版直接使用 images/brand 內的獨立 JPG。
 // 保留龜鹿飲 30cc 玻璃瓶與 180cc 鋁袋兩種規格。
-document.write('<script src="site-core.js?v=317.2"><\/script>');
-document.write('<script src="site-fix-v317.js?v=317.2"><\/script>');
+document.write('<script src="site-core.js?v=318.0"><\/script>');
+document.write('<script src="site-fix-v317.js?v=318.0"><\/script>');
