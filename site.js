b146a545d02346876bf4c28b13db757c9b4e56c5
TS15825868 <tung314069@gmail.com>
2026-07-11 15:29:10 +0800

    修正網站小老闆圖片黑畫面：停用圖片精靈並改回獨立 JPG

--- site.js
+++ site.js
@@ -1,4 +1,5 @@
 "use strict";
 
-document.write('<script src="site-core.js?v=316.1"><\/script>');
-document.write('<script src="website-mascot-sprite.js?v=316.1"><\/script>');
+// 網站正式版直接使用 images/brand 內的獨立 JPG。
+// 不再使用 Blob／Base64 圖片精靈，避免 iPhone Safari 出現黑畫面。
+document.write('<script src="site-core.js?v=317.0"><\/script>');
