bcfc5659439dabfafb612aee8e4cfae14ec9622f
TS15825868 <tung314069@gmail.com>
2026-07-11 15:33:31 +0800

    載入網站小老闆獨立圖片修正 v317

--- site.js
+++ site.js
@@ -3,3 +3,4 @@
 // 網站正式版直接使用 images/brand 內的獨立 JPG。
 // 不再使用 Blob／Base64 圖片精靈，避免 iPhone Safari 出現黑畫面。
 document.write('<script src="site-core.js?v=317.0"><\/script>');
+document.write('<script src="site-fix-v317.js?v=317.0"><\/script>');
