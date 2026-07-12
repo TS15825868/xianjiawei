f06d1e2180325beda075ca5f32fe266306a822e0
TS15825868 <tung314069@gmail.com>
2026-07-13 00:29:43 +0800

    切換官網至14張核准小老闆正式版本 v405.0

--- site.js
+++ site.js
@@ -1,7 +1,7 @@
 "use strict";
 
-// v404.3：只載入一套網站核心與一套小老闆頁面配置，避免重複插入。
-document.write('<link rel="stylesheet" href="site-v321.css?v=404.3" data-site-version="404.3">');
-document.write('<link rel="stylesheet" href="site-v404.css?v=404.3" data-mascot-version="404.3">');
-document.write('<script src="site-core.js?v=404.3"><\/script>');
-document.write('<script src="mascot-v404.js?v=404.3"><\/script>');
+// v405.0：只載入一套網站核心與一套核准小老闆頁面配置，避免重複插入。
+document.write('<link rel="stylesheet" href="site-v321.css?v=405.0" data-site-version="405.0">');
+document.write('<link rel="stylesheet" href="site-v405.css?v=405.0" data-mascot-version="405.0">');
+document.write('<script src="site-core.js?v=405.0"><\/script>');
+document.write('<script src="approved-mascot-v405.js?v=405.0"><\/script>');
