dde141eb881e25443b200258a381ee1865aed23b
TS15825868 <tung314069@gmail.com>
2026-07-12 19:04:06 +0800

    強制首頁品牌主圖使用 402.0 新快取

--- site.js
+++ site.js
@@ -4,3 +4,9 @@
 document.write('<link rel="stylesheet" href="site-v321.css?v=402.0" data-site-version="402.0">');
 document.write('<script src="site-fix-v317.js?v=402.0"><\/script>');
 document.write('<script src="site-core.js?v=402.0"><\/script>');
+
+document.addEventListener('DOMContentLoaded', () => {
+  document.querySelectorAll('img[src^="images/hero/home-brand-guilu-series.jpg"]').forEach((image) => {
+    image.src = 'images/hero/home-brand-guilu-series.jpg?v=402.0';
+  });
+});
