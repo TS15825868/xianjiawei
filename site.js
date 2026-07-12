adc3157fba776daf2d37062a39fb2c00a62ca377
TS15825868 <tung314069@gmail.com>
2026-07-12 22:44:57 +0800

    更新官網核心載入版本 v404.3

--- site.js
+++ site.js
@@ -1,7 +1,7 @@
 "use strict";
 
-// v404.2：只載入一套網站核心與一套小老闆頁面配置，避免重複插入。
-document.write('<link rel="stylesheet" href="site-v321.css?v=404.2" data-site-version="404.2">');
-document.write('<link rel="stylesheet" href="site-v404.css?v=404.2" data-mascot-version="404.2">');
-document.write('<script src="site-core.js?v=404.2"><\/script>');
-document.write('<script src="mascot-v404.js?v=404.2"><\/script>');
+// v404.3：只載入一套網站核心與一套小老闆頁面配置，避免重複插入。
+document.write('<link rel="stylesheet" href="site-v321.css?v=404.3" data-site-version="404.3">');
+document.write('<link rel="stylesheet" href="site-v404.css?v=404.3" data-mascot-version="404.3">');
+document.write('<script src="site-core.js?v=404.3"><\/script>');
+document.write('<script src="mascot-v404.js?v=404.3"><\/script>');
